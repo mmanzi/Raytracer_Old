352d282c55649f6540fafda2da6f01ee4dccab61
unknown <Administrator@romero.cgg.unibe.ch>
2013-09-10 13:34:21 +0200

    added rectangle, fixed pinhole camera (coordinate system)

--- GUI/Run.java
+++ GUI/Run.java
@@ -10,11 +10,9 @@ public class Run {
 	 */
 
 	public static void main(String[] args) {
-		
 		World myWorld = new TestScene();
 		RGBColor[][] rendered_image = myWorld.render_scene();
 		new GUI(rendered_image);
-		
 	}
 
 }
